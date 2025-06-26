29ac2b28e379cbd8228e1d3db1efb3b5ee7a97ae
Zachary Sexton <47196674+zasexton@users.noreply.github.com>
2025-06-26 10:50:55 -0700

    launching test subfolder for pytests

--- setup.py
+++ setup.py
@@ -554,7 +554,7 @@ extensions = [
 ]
 
 def read_version():
-    init_path = Path(__file__).parent / "yourpackage" / "__init__.py"
+    init_path = Path(__file__).parent / "svv" / "__init__.py"
     src = init_path.read_text(encoding="utf-8")
     match = re.search(r'^__version__\s*=\s*[\'"]([^\'"]+)[\'"]', src, re.M)
     if match:
