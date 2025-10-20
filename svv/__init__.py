ec967f682142570033b8cee07befba930b34392f
Zack Sexton <zsexton@stanford.edu>
2025-10-20 12:51:30 -0700

    updating build version to 0.0.34

--- svv/__init__.py
+++ svv/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.33"
+__version__ = "0.0.34"
 
 # If the optional companion package with compiled accelerators is installed
 # (svv-accelerated), prefer those modules transparently by aliasing them into
