2838b187211974fad47484f52b433998af52d80f
Zachary Sexton <47196674+zasexton@users.noreply.github.com>
2025-11-03 14:05:22 -0800

    Update __init__.py
    update version of svv

--- svv/__init__.py
+++ svv/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.34"
+__version__ = "0.0.35"
 
 # If the optional companion package with compiled accelerators is installed
 # (svv-accelerated), prefer those modules transparently by aliasing them into
